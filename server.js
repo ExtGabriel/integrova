485d36196c72b13eb38c16538a2e7aefa75469aa
Mariángel Chitay Duarte <84152916+Mariangel16@users.noreply.github.com>
2026-05-11 10:09:21 -0600

    cambio

--- server.js
+++ server.js
@@ -234,6 +234,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(express.json({ limit: '50mb' })); // Aumentar límite para archivos
 app.use(express.static(path.join(__dirname, 'CFE INSIGHT/App'))); // Servir archivos estáticos
 
